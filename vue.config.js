9726614a8f783ff4f9645295217040641dba494a
rbackrock <cokekokotea@gmail.com>
2021-05-28 15:58:17 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 'use strict'
 
-const name = 'vue3 脚手架'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 
 module.exports = {
   publicPath: '/',
@@ -30,5 +34,36 @@ module.exports = {
         }
       }
     }
+  },
+  chainWebpack(config) {
+    config
+    .when(process.env.NODE_ENV !== 'development',
+      config => {
+        config
+        .optimization.splitChunks({
+          chunks: 'all',
+          cacheGroups: {
+            libs: {
+              name: 'chunk-libs',
+              test: /[\\/]node_modules[\\/]/,
+              priority: 10,
+              chunks: 'initial' // 仅打包初始化中的第三方依赖包
+            },
+            antDesignVue: {
+              name: 'chunk-ant-design-vue',
+              priority: 20,
+              test: /[\\/]node_modules[\\/]_?ant-design-vue(.*)/
+            },
+            commons: {
+              name: 'chunk-commons',
+              test: resolve('src/components'),
+              minChunks: 3,
+              priority: 5,
+              reuseExistingChunk: true
+            }
+          }
+        })
+      }
+    )
   }
 }
