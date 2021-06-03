83287330216d093f85c422eb7a058fe4a83afd3e
rbackrock <cokekokotea@gmail.com>
2021-06-03 17:20:50 +0800

    # 完善深蓝自定义主题

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="less">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,14 @@
 <template>
-  <div class="home">
-    <a-menu
-      id="dddddd"
-      style="width: 256px"
-      v-model:openKeys="openKeys"
-      v-model:selectedKeys="selectedKeys"
-      mode="inline"
-      @click="handleClick"
-    >
-      <a-sub-menu key="sub1" @titleClick="titleClick">
-        <template #title>
-          <span>
-            <MailOutlined />
-            <span>Navigation One</span>
-          </span>
-        </template>
-        <a-menu-item-group key="g1">
-          <template #title>
-            <QqOutlined />
-            <span>Item 1</span>
-          </template>
-          <a-menu-item key="1">Option 1</a-menu-item>
-          <a-menu-item key="2">Option 2</a-menu-item>
-        </a-menu-item-group>
-        <a-menu-item-group key="g2" title="Item 2">
-          <a-menu-item key="3">Option 3</a-menu-item>
-          <a-menu-item key="4">Option 4</a-menu-item>
-        </a-menu-item-group>
-      </a-sub-menu>
-      <a-sub-menu key="sub2" @titleClick="titleClick">
-        <template #title>
-          <span>
-            <AppstoreOutlined />
-            <span>Navigation Two</span>
-          </span>
-        </template>
-        <a-menu-item key="5">Option 5</a-menu-item>
-        <a-menu-item key="6">Option 6</a-menu-item>
-        <a-sub-menu key="sub3" title="Submenu">
-          <a-menu-item key="7">Option 7</a-menu-item>
-          <a-menu-item key="8">Option 8</a-menu-item>
-        </a-sub-menu>
-      </a-sub-menu>
-      <a-sub-menu key="sub4">
-        <template #title>
-          <span>
-            <SettingOutlined />
-            <span>Navigation Three</span>
-          </span>
-        </template>
-        <a-menu-item key="9">Option 9</a-menu-item>
-        <a-menu-item key="10">Option 10</a-menu-item>
-        <a-menu-item key="11">Option 11</a-menu-item>
-        <a-menu-item key="12">Option 12</a-menu-item>
-      </a-sub-menu>
-    </a-menu>
-  </div>
-  <!-- -->
-  <div style="padding: 20px;">
-    <a-button type="primary">Primary</a-button><br><br>
-    <a-button>Default</a-button><br><br>
-    <a-button type="dashed">Dashed</a-button><br><br>
-    <a-button type="danger">Danger</a-button><br><br>
-    <a-config-provider :auto-insert-space-in-button="false"><br><br>
-      <a-button type="primary">按钮</a-button>
-    </a-config-provider>
-  </div>
+  <a-checkbox v-model:checked="checked">Checkbox</a-checkbox>
 </template>
-
 <script>
-export default {
-  name: 'Home',
-  components: {
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {
+      checked: ref(false)
+    }
   }
-}
+})
 </script>
