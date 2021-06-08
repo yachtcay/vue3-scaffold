f53dfb77ed09ff50b60be97fc179b73495ade121
rbackrock <cokekokotea@gmail.com>
2021-06-08 14:16:17 +0800

    添加常见 http 错误码信息页面

---
+++ src/views/features/401.vue
@@ -0,0 +1,7 @@
+<template>
+  <a-result status="404" title="404" sub-title="对不起，没有此页面">
+    <template #extra>
+      <a-button type="primary">回到首页</a-button>
+    </template>
+  </a-result>
+</template>
