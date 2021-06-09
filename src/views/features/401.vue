9cb8a7a0f82efc9205c823791ca752fae924c097
rbackrock <cokekokotea@gmail.com>
2021-06-09 16:23:05 +0800

    删除多余页面

--- src/views/features/401.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <a-result status="404" title="404" sub-title="对不起，没有此页面">
-    <template #extra>
-      <a-button type="primary">回到首页</a-button>
-    </template>
-  </a-result>
-</template>
