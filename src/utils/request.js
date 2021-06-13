b9a27aa8ef136ec6a3f1ece9a9dfa3bea59b0e9e
rbackrock <cokekokotea@gmail.com>
2021-06-13 22:21:53 +0800

    初步封装 axios

---
+++ src/utils/request.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+// 约定的业务状态码
+const BUSINESS_CODE_SUCCESS = 20000
+
+const axiosInstance = axios.create({
+  baseURL: process.env.VUE_APP_REQUEST_BASE,
+  timeout: 6000
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  return config
+}, (error) => {
+  console.error(error)
+})
+
+axiosInstance.interceptors.response.use((response) => {
+  const rspData = response.data
+
+  if (rspData.code !== BUSINESS_CODE_SUCCESS) {
+    return Promise.reject(new Error(rspData.message || 'Error'))
+  }
+
+  return {
+    result: rspData,
+    data: rspData.data
+  }
+})
+
+export default axiosInstance
