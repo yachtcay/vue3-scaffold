f3677e594a3dac1a7b8259d1f587da65f850c2e6
rbackrock <cokekokotea@gmail.com>
2021-06-14 15:57:36 +0800

    添加新示例路由

--- src/router/router-table.js
+++ src/router/router-table.js
@@ -1,11 +1,19 @@
 import MainLayout from '@/layouts/MainLayout'
+import Home from '@/views/home'
+import Error404 from '@/views/features/404'
+import Login from '@/views/login'
 
 // feature
 const feature = [
   {
     path: '/404',
     name: 'NotFound',
-    component: () => import('@/views/features/404')
+    component: Error404
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
@@ -14,11 +22,58 @@ const base = [
   {
     path: '/',
     component: MainLayout,
-    redirect: '/home',
+    redirect: {
+      name: 'Home'
+    },
     children: [
       {
         path: 'home',
-        component: () => import('@/views/home')
+        name: 'Home',
+        component: Home
+      }
+    ]
+  }
+]
+
+// 系统路由
+const system = [
+  {
+    path: '/example',
+    component: MainLayout,
+    redirect: '/example/example1',
+    children: [
+      {
+        path: 'example1',
+        name: 'Example1',
+        component: () => import('@/views/business/example/example1')
+      }
+    ]
+  }
+]
+
+// 业务路由
+const example = [
+  {
+    path: '/example',
+    component: MainLayout,
+    redirect: {
+      name: 'Example1'
+    },
+    children: [
+      {
+        path: 'example1',
+        name: 'Example1',
+        component: () => import('@/views/business/example/example1')
+      },
+      {
+        path: 'example2',
+        name: 'Example2',
+        component: () => import('@/views/business/example/example2')
+      },
+      {
+        path: 'example3',
+        name: 'Example3',
+        component: () => import('@/views/business/example/example3')
       }
     ]
   }
@@ -32,4 +87,7 @@ const end = [
   }
 ]
 
-export default feature.concat(base, end)
+export const systemRoutes = feature.concat(base, end)
+export const business = example
+
+export default feature.concat(base, business, end)
