7a18684e0403e7263ed98a331b65d4159863bd8f
rbackrock <cokekokotea@gmail.com>
2021-06-15 22:48:33 +0800

    构造基本 vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import { createStore } from 'vuex'
 
+/**
+ * 利用 webpack 的 require.context 能力，获取 modules 文件下的所有 .js 文件导出
+ */
+const modulesFiles = require.context('./modules', true, /\.js$/)
+const modules = modulesFiles.keys().reduce((module, modulePath) => {
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  // eslint-disable-next-line no-param-reassign
+  module[moduleName] = value.default
+  return module
+}, {})
+
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  modules
 })
