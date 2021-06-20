3b9195f8af3e5520336a3a51bf06daf8da7a49aa
rbackrock <cokekokotea@gmail.com>
2021-06-20 23:29:17 +0800

    添加系统对路由和菜单的 store

--- src/store/modules/system.js
+++ src/store/modules/system.js
@@ -1,13 +1,39 @@
+import * as api from '@/api/system'
+import { getRspRouteTableKeys } from '@/router/helper'
+
 const state = () => ({
+  navigationMenu: [],
+  requiresAuthRoutesOfKeys: []
 })
 
 const getters = {
+  authRouteKeys(state) {
+    return state.requiresAuthRoutesOfKeys
+  }
 }
 
 const mutations = {
+  SET_NAVIGATION_MENU(state, menu) {
+    state.navigationMenu = menu
+  },
+
+  SET_REQUIRES_AUTH_ROUTES_OF_KEYS(state, keys) {
+    state.requiresAuthRoutesOfKeys = keys
+  }
 }
 
 const actions = {
+  async buildNavigationMenu({ commit }) {
+    try {
+      const { data } = await api.queryMenu()
+      commit('SET_NAVIGATION_MENU', data)
+      commit('SET_REQUIRES_AUTH_ROUTES_OF_KEYS', getRspRouteTableKeys(data))
+
+      return Promise.resolve(data)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
 }
 
 export default {
