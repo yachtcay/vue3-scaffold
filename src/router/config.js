30917e3cb59cf150d2c57165408fcb306f205293
rbackrock <cokekokotea@gmail.com>
2021-06-20 11:38:44 +0800

    重构路由守卫为 v4 版本推荐的方法

--- src/router/config.js
+++ src/router/config.js
@@ -11,56 +11,51 @@ const routesNameWhiteList = [
 
 /**
  * 前置路由守卫
- * 路由守卫跳转页面是 next 方法，为了方便多种顺序判断，
- * 额外增加 return 'done' 使函数按照 next() 执行位置结束，
- * 类似标识，没有任何含义，只是为了结束函数执行
+ * v4 版本路由只需要 return 即可，不需要 next 但是也保留有
  */
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async (to) => {
   NProgress.start()
 
   const token = store.getters['auth/token']
   const user = store.getters['user/userInfo']
 
-  // 路由白名单中如果有直接跳转目标页面
-  if (routesNameWhiteList.indexOf(to.name) !== -1) {
-    next()
-    return 'done'
+  // 路由白名单中如果有，直接跳转目标页面
+  if (!token && routesNameWhiteList.indexOf(to.name) !== -1) {
+    return true
   }
 
   // 如果没有令牌，直接跳转登录页面
   if (!token) {
-    next({
-      name: 'Login'
-    })
-
-    return 'done'
+    return {
+      name: 'Login',
+      params: {
+        message: '登录状态已失效，请先登录'
+      }
+    }
   }
 
   // 如果没有用户信息，则可能是已经登录成功，但是用户刷新了页面，此时重新拉取必要数据即可
   if (!user) {
     try {
       await store.dispatch('user/getUser')
-
-      next({ ...to, replace: true })
-      return 'done'
     } catch (error) {
-      next({
-        name: 'Login'
-      })
-      return 'done'
+      return {
+        name: 'Login',
+        params: {
+          message: '发生错误，请联系管理员'
+        }
+      }
     }
   }
 
-  // 到这里有个特殊情况，在令牌和用户信息都存在的情况下，如果访问首页，直接跳转到 Home 页
+  // 到这里有个特殊情况，在令牌和用户信息都存在的情况下，如果访问首页，直接跳转到 Home 页，因为 Login 页面特殊性放在第一个判断
   if (to.name === 'Login') {
-    next({
+    return {
       name: 'Home'
-    })
-    return 'done'
+    }
   }
 
-  next()
-  return 'done'
+  return true
 })
 
 // 后置
