fd140bf77a10fa9e405dadb76bad34295a35c0c4
rbackrock <cokekokotea@gmail.com>
2021-06-20 11:37:43 +0800

    添加 404 页面回到首页功能

--- src/views/features/404.vue
+++ src/views/features/404.vue
@@ -1,7 +1,20 @@
 <template>
   <a-result status="404" title="404" sub-title="对不起，没有此页面">
     <template #extra>
-      <a-button type="primary">回到首页</a-button>
+      <a-button type="primary" @click="handleBackHome">回到首页</a-button>
     </template>
   </a-result>
 </template>
+
+<script>
+export default {
+  name: 'NotFount',
+  methods: {
+    handleBackHome() {
+      this.$router.replace({
+        name: 'Home'
+      })
+    }
+  }
+}
+</script>
