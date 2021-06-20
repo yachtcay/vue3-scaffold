3083747dd17fa831dd1f61cbaa7c0d8ef3dbaaef
rbackrock <cokekokotea@gmail.com>
2021-06-20 11:39:40 +0800

    将首页路由判定为基本路由分类

--- src/router/router-table.js
+++ src/router/router-table.js
@@ -1,3 +1,8 @@
+/**
+ * 新版文档中，路由数组中的顺序并不重要
+ * https://next.router.vuejs.org/zh/guide/essentials/route-matching-syntax.html#%E5%9C%A8%E5%8F%82%E6%95%B0%E4%B8%AD%E8%87%AA%E5%AE%9A%E4%B9%89%E6%AD%A3%E5%88%99
+ */
+
 import MainLayout from '@/layout/MainLayout'
 import Error404 from '@/views/features/404'
 import Login from '@/views/login'
@@ -17,15 +22,7 @@ const base = [
     path: '/login',
     name: 'Login',
     component: Login
-  }
-]
-
-// 系统路由
-const system = [
-]
-
-// 业务路由
-const example = [
+  },
   {
     path: '/',
     component: MainLayout,
@@ -40,6 +37,20 @@ const example = [
       }
     ]
   },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: 'NotFound'
+    }
+  }
+]
+
+// 系统路由
+const system = [
+]
+
+// 业务路由
+const example = [
   {
     path: '/example',
     component: MainLayout,
@@ -68,12 +79,6 @@ const example = [
 
 // 路由匹配的末尾
 const end = [
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: {
-      name: 'NotFound'
-    }
-  }
 ]
 
 export {
