7c7cea2ed939cba33413f787e6233c57a537901c
rbackrock <cokekokotea@gmail.com>
2021-06-20 23:25:50 +0800

    添加示例页面，测试嵌套路由

---
+++ src/views/business/example/example4/layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>example4</div>
+  <div>
+    <router-link :to="{ name: 'Example4List' }">List</router-link>
+    <router-link :to="{ name: 'Example4Detail' }">Detail</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'Example4Layout'
+}
+</script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <a-button>Test</a-button>
+  <a-button @click="handleClick">Test</a-button>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {
+  },
+  methods: {
+    handleClick() {
+      this.$router.push({
+        name: 'Example'
+      })
+    }
   }
 }
 </script>
