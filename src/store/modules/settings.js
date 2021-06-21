79e9d42b5d3b1cd422df006711e053897bb63763
rbackrock <cokekokotea@gmail.com>
2021-06-21 17:24:01 +0800

    添加 settings 的 store

---
+++ src/store/modules/settings.js
@@ -0,0 +1,22 @@
+import { layoutType } from '@/consts'
+
+const state = () => ({
+  layoutType: layoutType.SIDE_MENU
+})
+
+const getters = {
+}
+
+const mutations = {
+}
+
+const actions = {
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
