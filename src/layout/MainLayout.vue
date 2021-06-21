f06874c82aa456d7870fe01e7737e11d055fdaaf
rbackrock <cokekokotea@gmail.com>
2021-06-21 17:24:44 +0800

    完善导航组件

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,11 +1,24 @@
 <template>
   <!-- sider menu -->
-  <a-layout class="main-layout-container">
+  <a-layout v-if="isSideMenu" class="side-menu-main-layout-container">
     <a-layout-sider>
-      <navigation />
+      <div class="logo">logo</div>
+      <div class="navigation">
+        <navigation />
+      </div>
+      <div class="collapse">
+        <a-button type="primary" @click="toggleCollapsed" style="margin-bottom: 16px" />
+      </div>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header>Header</a-layout-header>
+      <a-layout-header>
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
       <a-layout-content>
         <router-view></router-view>
       </a-layout-content>
@@ -14,37 +27,71 @@
   </a-layout>
 
   <!-- header menu -->
-  <!-- <a-layout>
+  <a-layout v-if="isTopMenu">
     <a-layout-header>Header</a-layout-header>
     <a-layout-content>Content</a-layout-content>
     <a-layout-footer>Footer</a-layout-footer>
-  </a-layout> -->
+  </a-layout>
 
   <!-- mix menu -->
-  <!-- <a-layout>
+  <a-layout v-if="isMixMenu">
     <a-layout-header>Header</a-layout-header>
     <a-layout>
       <a-layout-sider>Sider</a-layout-sider>
       <a-layout-content>Content</a-layout-content>
     </a-layout>
     <a-layout-footer>Footer</a-layout-footer>
-  </a-layout> -->
+  </a-layout>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { layoutType as layoutTypeConsts } from '@/consts'
 import Navigation from './components/TheNavigation'
 
 export default {
   name: 'MainLayout',
   components: {
     Navigation
+  },
+  computed: {
+    ...mapState('settings', {
+      isSideMenu: (state) => state.layoutType === layoutTypeConsts.SIDE_MENU,
+      isTopMenu: (state) => state.layoutType === layoutTypeConsts.TOP_MENU,
+      isMixMenu: (state) => state.layoutType === layoutTypeConsts.MIX_MENU
+    })
+  },
+  data() {
+    return {
+      collapsed: false
+    }
   }
 }
 </script>
 
 <style lang="less">
-  .main-layout-container {
-    height: 100vh;
+  .side-menu-main-layout-container {
     color: #fff;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+
+    .logo {
+      position: relative;
+      display: flex;
+      align-items: center;
+      padding: 16px 16px;
+      line-height: 32px;
+      cursor: pointer;
+    }
+
+    .navigation {
+      flex: 1 1 0%;
+      overflow: hidden auto;
+    }
+
+    .collapse {
+      width: 100%;
+    }
   }
 </style>
--- src/layout/components/TheNavigation.vue
+++ src/layout/components/TheNavigation.vue
@@ -1,9 +1,109 @@
 <template>
-  navigation
+  <a-menu
+    id="dddddd"
+    style="width: 100%; height: 100%;"
+    v-model:openKeys="openKeys"
+    v-model:selectedKeys="selectedKeys"
+    mode="inline"
+    theme="dark"
+    @click="handleClick"
+  >
+    <a-sub-menu key="sub1" @titleClick="titleClick">
+      <template #title>
+        <span>
+          <MailOutlined />
+          <span>Navigation One</span>
+        </span>
+      </template>
+      <a-menu-item-group key="g1">
+        <template #title>
+          <QqOutlined />
+          <span>Item 1</span>
+        </template>
+        <a-menu-item key="a1">Option 1</a-menu-item>
+        <a-menu-item key="a2">Option 2</a-menu-item>
+      </a-menu-item-group>
+      <a-menu-item-group key="g2" title="Item 2">
+        <a-menu-item key="a3">Option 3</a-menu-item>
+        <a-menu-item key="a4">Option 4</a-menu-item>
+      </a-menu-item-group>
+    </a-sub-menu>
+    <a-sub-menu key="sub2" @titleClick="titleClick">
+      <template #title>
+        <span>
+          <AppstoreOutlined />
+          <span>Navigation Two</span>
+        </span>
+      </template>
+      <a-menu-item key="5">Option 5</a-menu-item>
+      <a-menu-item key="6">Option 6</a-menu-item>
+      <a-sub-menu key="sub3" title="Submenu">
+        <a-menu-item key="7">Option 7</a-menu-item>
+        <a-menu-item key="8">Option 8</a-menu-item>
+      </a-sub-menu>
+    </a-sub-menu>
+    <a-sub-menu key="sub4">
+      <template #title>
+        <span>
+          <SettingOutlined />
+          <span>Navigation Three</span>
+        </span>
+      </template>
+      <a-menu-item key="9">Option 9</a-menu-item>
+      <a-menu-item key="10">Option 10</a-menu-item>
+      <a-menu-item key="11">Option 11</a-menu-item>
+      <a-menu-item key="12">Option 12</a-menu-item>
+    </a-sub-menu>
+    <a-sub-menu key="sub5">
+      <template #title>
+        <span>
+          <SettingOutlined />
+          <span>Navigation Three</span>
+        </span>
+      </template>
+      <a-menu-item key="c9">Option 9</a-menu-item>
+      <a-menu-item key="c10">Option 10</a-menu-item>
+      <a-menu-item key="c11">Option 11</a-menu-item>
+      <a-menu-item key="c12">Option 12</a-menu-item>
+    </a-sub-menu>
+  </a-menu>
 </template>
 
 <script>
+import { ref } from 'vue'
+import {
+  MailOutlined,
+  QqOutlined,
+  AppstoreOutlined,
+  SettingOutlined
+} from '@ant-design/icons-vue'
+
 export default {
-  name: 'TheNavigation'
+  name: 'TheNavigation',
+  components: {
+    MailOutlined,
+    QqOutlined,
+    AppstoreOutlined,
+    SettingOutlined
+  },
+  setup() {
+    const selectedKeys = ref(['1'])
+    const openKeys = ref(['sub1'])
+
+    const handleClick = (e) => {
+      console.log('click', e)
+    }
+
+    const titleClick = (e) => {
+      console.log('titleClick', e)
+    }
+
+    return {
+      selectedKeys,
+      openKeys,
+      handleClick,
+      titleClick
+    }
+  }
 }
 </script>
