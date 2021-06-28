abb4b14615b1a101f6dc835e66042d9d750eff9e
rbackrock <cokekokotea@gmail.com>
2021-06-28 14:29:28 +0800

    独立出菜单图标的映射关系，重新实现为菜单追加图标的逻辑方法

--- src/router/helper.js
+++ src/router/helper.js
@@ -1,21 +1,23 @@
 import * as utils from '@/utils/helper'
-import { requiresAuthRoutes } from './router-table'
-
-// 使导航菜单附加图标组件
+import iconMapperData from './icon-mapper'
 
 /**
- * 获取附加了图标组件的菜单
- * @param {Array} pickIconMapper 获取带图标的数
- * @param {Array}} rspMenuTable 后端传入的菜单
- * @returns 附加了图标组件的菜单
+ * 获得追加菜单图标的菜单树
+ * @param {Array} rspMenuTable 后端传入的菜单树
+ * @param {Object} iconMapper 菜单和菜单图标的映射对象
+ * @returns 添加图标后的菜单树
  */
-function attachIcon(pickIconMapper, rspMenuTable) {
+function attachIcon(rspMenuTable = [], iconMapper) {
   for (let i = 0; i < rspMenuTable.length; i++) {
     const current = rspMenuTable[i]
-    current.icon = pickIconMapper[current.routeName] || null
+    if ('uniqueTag' in current) {
+      if (iconMapper[current.uniqueTag]) {
+        current.icon = iconMapper[current.uniqueTag]
+      }
+    }
 
     if ('children' in current && current.children.length > 0) {
-      attachIcon(pickIconMapper, current.children)
+      attachIcon(current.children, iconMapper)
     }
   }
 
@@ -62,11 +64,7 @@ function getMenuFlat(menu, includeType = [], filterFn = () => true) {
 export const getRspMenuTableKeys = (rspMenuTable = []) => utils.pickPropertyOfTree(rspMenuTable, 'routeName', 'routeName')
 
 // 构成导航菜单，其中从路由表中附加图标组件
-export const makeNavigationMenu = (rspMenuTable = []) => {
-  const routeTable = requiresAuthRoutes
-  const pickIconMapper = utils.pickPropertyOfTree(routeTable, 'icon', 'name')
-  return attachIcon(pickIconMapper, rspMenuTable)
-}
+export const makeNavigationMenu = (rspMenuTable = []) => attachIcon(rspMenuTable, iconMapperData)
 
 // 为了菜单导航，只获得 type 为 menu 的 routenName 一维数组，其中过滤了当 type = menu 时，还有 children 的菜单
 export const getNavigationOnlyMenuFlat = (navigationMenu = []) => getMenuFlat(navigationMenu, ['menu'], (menu, includeType, currentMenu) => currentMenu.type !== 'menu')
---
+++ src/router/icon-mapper.js
@@ -0,0 +1,13 @@
+import {
+  StarOutlined
+} from '@ant-design/icons-vue'
+
+export default {
+  Home: StarOutlined,
+  Example: StarOutlined,
+  Example1: StarOutlined,
+  Example2: StarOutlined,
+  Example3: StarOutlined,
+  Example4: StarOutlined,
+  Example5: StarOutlined
+}
