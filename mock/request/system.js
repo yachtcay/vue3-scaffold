dbb4c29ce636a7379bc0191a348606f338e979dd
rbackrock <cokekokotea@gmail.com>
2021-06-28 14:29:48 +0800

    为菜单图标添加额外 uniqueTag 字段

--- mock/request/system.js
+++ mock/request/system.js
@@ -8,35 +8,41 @@ module.exports = [
         {
           routeName: 'Home',
           title: '首页',
+          uniqueTag: 'Home',
           type: 'menu',
           children: []
         },
         {
-          routeName: 'Example',
+          routeName: '',
           title: 'Example',
+          uniqueTag: 'Example',
           type: 'catalog',
           children: [
             {
               routeName: 'Example1',
               title: 'Example1',
+              uniqueTag: 'Example1',
               type: 'menu',
               children: []
             },
             {
               routeName: 'Example2',
               title: 'Example2',
+              uniqueTag: 'Example2',
               type: 'menu',
               children: []
             },
             {
               routeName: 'Example3',
               title: 'Example3',
+              uniqueTag: 'Example3',
               type: 'menu',
               children: []
             },
             {
               routeName: 'Example4',
               title: 'Example4',
+              uniqueTag: 'Example4',
               type: 'menu',
               children: [
                 {
@@ -56,40 +62,17 @@ module.exports = [
             {
               routeName: '',
               title: 'Example5',
+              uniqueTag: 'Example5Group',
               type: 'group',
               children: [
                 {
                   routeName: 'Example5',
                   title: 'Example5',
+                  uniqueTag: 'Example5',
                   type: 'menu',
                   children: []
                 }
               ]
-            }
-          ]
-        },
-        {
-          routeName: 'Business',
-          title: 'Business',
-          type: 'catalog',
-          children: [
-            {
-              routeName: 'Business1',
-              title: 'Business1',
-              type: 'menu',
-              children: []
-            },
-            {
-              routeName: 'Business2',
-              title: 'Business2',
-              type: 'menu',
-              children: []
-            },
-            {
-              routeName: 'Business3',
-              title: 'Business3',
-              type: 'menu',
-              children: []
             }
           ]
         }
