fcb4e913390bbe15ad69414b86a26db4223c496f
rbackrock <cokekokotea@gmail.com>
2021-06-29 16:22:55 +0800

    添加菜单图标映射

--- src/router/icon-mapper.js
+++ src/router/icon-mapper.js
@@ -9,5 +9,21 @@ export default {
   Example2: StarOutlined,
   Example3: StarOutlined,
   Example4: StarOutlined,
-  Example5: StarOutlined
+  Example5: StarOutlined,
+  Business: StarOutlined,
+  Business1Catalog: StarOutlined,
+  Business1: StarOutlined,
+  Business11: StarOutlined,
+  Business2: StarOutlined,
+  Business21: StarOutlined,
+  Business3: StarOutlined,
+  Business31: StarOutlined,
+  Business4: StarOutlined,
+  Business41: StarOutlined,
+  Business5: StarOutlined,
+  Business51: StarOutlined,
+  Business6: StarOutlined,
+  Business61: StarOutlined,
+  Business7: StarOutlined,
+  Business71: StarOutlined
 }
