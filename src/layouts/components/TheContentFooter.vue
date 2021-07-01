111050414c01e3d6e807c30eaea763f0c3ce46a2
rbackrock <cokekokotea@gmail.com>
2021-07-01 14:29:32 +0800

    添加 footer 组件

---
+++ src/layouts/components/TheContentFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer-container">Copyright  vue3-scaffold</div>
+</template>
+
+<script>
+export default {
+  name: 'ContentFooter'
+}
+</script>
+
+<style lang="less" scoped>
+  .footer-container {
+    color: rgba(0,0,0,.45);
+    font-size: 14px;
+    text-align: center;
+  }
+</style>
